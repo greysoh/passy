a2d7b8bba62dcc0e225b412dc61b98dfd0e51400
greysoh <greysoh@pm.me>
2022-08-18 15:20:03 -0400

    fix: Makes package publishable.

--- examples/test_implementation/index.js
+++
@@ -1,9 +0,0 @@
-const PassyServer = require("../../index.js");
-const options = require("../../options.json");
-
-async function main() {
-  const proxy = new PassyServer(options);
-  await proxy.main();
-}
-
-main();
